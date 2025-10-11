1ac1102132118e4b38fff1deea19c8148d97d21a
Andre Coullard <andrecoullard@gmail.com>
2025-10-11 19:51:27 -0400

    add plan for api routes

---
+++ src/app/api/image-gen/route.js
@@ -0,0 +1,9 @@
+
+
+export async function POST(req) {
+    // get info on the current room from the world data
+    // generate an image based on that info
+    // convert that image to ascii and send back text.
+
+
+}
