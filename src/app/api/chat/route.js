7fe89516e57283b0ea243f5be4e0f2f1b0a88336
Andre Coullard <andrecoullard@gmail.com>
2025-10-11 19:58:18 -0400

    comlete plan for route

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,8 +1,14 @@
 import { google } from '@ai-sdk/google';
 import { streamText, convertToModelMessages } from 'ai';
 
+
+// place holder function which just feeds the message history straight into
+// gemini and sends back the results in the proper format.
 export async function POST(req) {
-  const { messages } = await req.json();
+  const { messages } = await req.json(); // a list of all messages so far
+
+  // this is the most recent message
+  console.log("last message: ", messages[messages.length - 1]);
 
   const resultStream = await streamText({
     model: google('gemini-2.0-flash-lite'),
@@ -12,7 +18,9 @@ export async function POST(req) {
   return resultStream.toUIMessageStreamResponse();
 }
 
+// ACTUAL FUNCTION PLAN
 
-
-// return a text description to output. This is what the user will see
-// return a tag for what type of action was taken.
+// inputs: All messages so far
+// outputs:
+//  description: a text description to output. This is what the user will see
+//  action-type: a tag for what type of action was taken, this should be from a list of defined types.
