a8486ac43e0921642325f26f87175182560f8cb1
yixuan h <kathelin_huang@163.com>
2025-10-11 20:11:10 -0400

    instruction matching module and allowed to set temperature

--- src/app/route_generator.py
+++ src/app/route_generator.py
@@ -4,20 +4,22 @@ API_KEY = "AIzaSyDGu_RLE9U2cBGsq9SRghxLEinBlLb6YQk"
 MODEL = "models/gemini-2.5-flash-lite"
 N = 6
 MESSAGE = f'generate a {N} by {N} array where the elements \
-    are from the list [P, T, R]. \
+    are from the list [P, Q]. \
     Must randomly specify one "starting point" labelled "S", \
-    and one "ending point" labelled "F", the two should not be seperated less than {N} entries. \
-    The "P" elements must be connected and form a pathfrom "S" to "F" (diagonals are allowed). \
+    and one "ending point" labelled "F" \
+    The "P" elements must and form a complete connected path from "S" to "F" (diagonals are allowed). \
+    The path length should be at least {N} entries long\
     Output this array in the format: """{{"map":[[], [], [], [], []]}}"""'
 MAP_PATH = r"backend_data\map.json"
 
-def ask_llm(key, model_id, messages, temperature=0.7):
+def ask_llm(key, model_id, messages, temperature=0.5):
     client = genai.Client(api_key=key)
     response = client.models.generate_content(
         model=model_id,
         contents=messages,
         config={
-            'response_mime_type': 'application/json'
+            'response_mime_type': 'application/json',
+            'temperature': temperature
     })
     return (response.text)
 
@@ -26,7 +28,7 @@ def store_map(filename, content):
         file.write(content)
 
 def main():
-    response = ask_llm(API_KEY,MODEL, MESSAGE)
+    response = ask_llm(API_KEY,MODEL, MESSAGE, 0.7)
     store_map(MAP_PATH, response)
 
 if __name__ == "__main__":
